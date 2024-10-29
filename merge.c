745ceaf10964c77aeafa348720df75b9c526a834
Capelosini <capelosini@gmail.com>
2024-10-29 20:53:29 -0300

    MergeSort Fix

--- merge.c
+++ merge.c
@@ -1,45 +1,58 @@
 #include "arrayOperations.h"
 
-void merge(int* arr, int astart, int aend, int bstart, int bend){
-    int arrayALen=aend-astart;
-    int arrayBLen=bend-bstart;
+int count=0;
+
+void merge(int* arr, int astart, int aend, int bstart, int bend) {
+    int arrayALen = aend - astart;
+    int arrayBLen = bend - bstart;
 
     int arrayA[arrayALen], arrayB[arrayBLen];
 
-    memcpy(arrayA, arr+astart, arrayALen*sizeof(int));
-    memcpy(arrayB, arr+bstart, arrayBLen*sizeof(int));
+    memcpy(arrayA, arr + astart, arrayALen * sizeof(int));
+    memcpy(arrayB, arr + bstart, arrayBLen * sizeof(int));
 
-    int i = 0, j = 0;
-    for (int t=astart; t<bend; t++){
-        if (j >= arrayBLen || arrayA[i]  <= arrayB[j]){
-            arr[t]=arrayA[i];
-            i++;
-        } else{
-            arr[t]=arrayB[j];
-            j++;
+    int i = 0, j = 0, k = astart;
+    while (i < arrayALen && j < arrayBLen) {
+        count++;
+        if (arrayA[i] <= arrayB[j]) {
+            arr[k++] = arrayA[i++];
+        } else {
+            arr[k++] = arrayB[j++];
         }
+    }
+
+    while (i < arrayALen) {
+        count++;
+        arr[k++] = arrayA[i++];
+    }
+
+    while (j < arrayBLen) {
+        count++;
+        arr[k++] = arrayB[j++];
     }
 }
 
 void mergeSort(int* arr, int start, int end){
-    if (end-start <= 1){
+    count++;
+    if (start >= end-1){
         return;
     }
     int m = start + (end-start)/2;
-    printf("\narrA: %d, %d | arrB: %d, %d\n", start, m, m, end);
     mergeSort(arr, start, m);
     mergeSort(arr, m, end);
 
     merge(arr, start, m, m, end);
+    printArray(arr, end);
 }
 
 int main(){
 
     int* arr = getTestArr();
     int len = getTestArrLength();
-
+    startTimer();
     mergeSort(arr, 0, len);
-    printArray(arr, len);
+    endTimer();
+    printf("Count: %d\n", count);
 
     return 0;
 }
--- test.sh
+++ test.sh
@@ -6,7 +6,13 @@ echo "Building InsertionSort..."
 gcc insertion.c arrayOperations.c -o insertion
 echo "Builded!"
 
+echo "Building MergeSort..."
+gcc merge.c arrayOperations.c -o merge
+echo "Builded!"
+
 echo "__BubbleSort__"
 ./bubble
 echo "__InsertionSort__"
 ./insertion
+echo "__MergeSort__"
+./merge
