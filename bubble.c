5130c08af26119c14781059903cd1dfa6bb6bfb8
Clovis A Pelosini <capelosini@gmail.com>
2024-10-30 01:22:22 -0300

    Add bubble sort recursive version

--- bubble.c
+++ bubble.c
@@ -23,10 +23,29 @@ void bubbleSort(int* arr, int n, unsigned char invert){
     printf("Count: %d\n\n", count);
 }
 
+void bubbleSortR(int* arr, int n, int i){
+    if (n == 1)
+        return;
+    if (i == n-1){
+        i=0;
+        n--;
+    }
+
+    bubbleSortR(arr, n, i+1);
+
+    if (arr[i] > arr[i+1]){
+        swap(arr, i, i+1);
+        printArray(arr, getTestArrLength());
+    }
+}
+
 int main(){
     int* arr = getTestArr();
-
-    bubbleSort(arr, getTestArrLength(), 0);
+    int len = getTestArrLength();
+    bubbleSort(arr, len, 0);
+    // recursive
+    //bubbleSortR(arr, len, 0);
+    //printArray(arr, len);
 
     return 0;
 }
